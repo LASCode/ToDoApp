845b1357cac2d2fcb9c928bacb9ed9a11e9c4d50
LASCode <geimixa123@gmail.com>
2022-06-27 22:47:48 +0300

    create js variables folder

---
+++ src/vriables/variables.ts
@@ -0,0 +1,16 @@
+import defaultUserAvatar from '../assets/img/defaultUserAvatar.png'
+const LSVariables = {
+  TASKS_STORAGE_NAME: 'LSTasks',
+  TOKEN_STORAGE_NAME: 'LSToken',
+  TASKS_STORAGE_DEFAULT_VALUE: [],
+  TOKEN_STORAGE_DEFAULT_VALUE: {key: ''},
+}
+
+const UserVariables = {
+  DEFAULT_USER_AVATAR: defaultUserAvatar,
+  DEFAULT_USER_NAME: 'Незарегистрированный пользователь',
+}
+
+
+
+export { LSVariables, UserVariables };

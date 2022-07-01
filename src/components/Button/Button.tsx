e3e612e68b81c8267fa61b41d8c4e71fd5445822
LASCode <geimixa123@gmail.com>
2022-07-01 23:53:18 +0300

    add disabled and loading params button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,18 @@
 import './Button.scss';
 import React from 'react';
 import { getClassnamesFromObject } from '../../features/get-classnames-from-object';
+import { ReactComponent as DotsSvg } from '../../assets/img/icon-3dots.svg'
 
 interface IButton extends React.ButtonHTMLAttributes<HTMLButtonElement>{
   children?: React.ReactNode,
   icon?: false | string | undefined,
   type?: 'submit' | 'reset' | 'button' | undefined,
   adaptive?: boolean,
+  loading?: boolean,
+  disabled?: boolean,
 }
 
-const Button = ({icon, children, type = 'button', adaptive = true, ...otherParams}: IButton) => {
+const Button = ({icon, children, type = 'button', adaptive = true, loading = false, disabled = false, ...otherParams}: IButton) => {
   const classNames = getClassnamesFromObject({
     'Button' : true,
     'Button--with-icon': icon,
@@ -17,9 +20,10 @@ const Button = ({icon, children, type = 'button', adaptive = true, ...otherParam
     'Button--with-adaptive': adaptive,
   });
   return (
-    <button className={classNames} type={type} {...otherParams}>
+    <button className={classNames} type={type} {...otherParams} disabled={disabled}>
       { icon && <img className='Button__icon' src={icon} alt='Button Icon'/> }
-      { children && <span className='Button__text'>{children}</span> }
+      { (children && !loading) && <span className='Button__text'>{children}</span> }
+      { loading && <DotsSvg className='Button__loading'/>}
     </button>
   );
 };

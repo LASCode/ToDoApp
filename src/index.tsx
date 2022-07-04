27eb35cbd6e0a4ec1b1010a1e315216faf7f4668
LASCode <geimixa123@gmail.com>
2022-07-04 23:25:14 +0300

    add setDefaultLocalstorage in index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,19 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import './declare_modules.d';
+import { setDefaultLocalStorage } from './features/set-default-local-storage';
 import './index.scss';
 import { setupStore } from './redux/store';
 import { asyncServerRequest, createFakeBackend } from './fakeBackend';
 import { BrowserRouter } from 'react-router-dom';
 
+
+createFakeBackend();
+setDefaultLocalStorage();
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 const store = setupStore();
-createFakeBackend();
+
 
 root.render(
   <Provider store={store}>

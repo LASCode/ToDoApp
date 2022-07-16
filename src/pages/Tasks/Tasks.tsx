212eb5391f36054fd7e6d8c5a42802a738cddfbe
LASCode <geimixa123@gmail.com>
2022-07-16 23:16:14 +0300

    remove autoheight import

--- src/pages/Tasks/Tasks.tsx
+++ src/pages/Tasks/Tasks.tsx
@@ -2,7 +2,6 @@ import './Tasks.scss';
 import React, { useState } from 'react';
 import CrossSvg from '../../assets/img/icon-add.svg';
 import AddTaskSection from './AddTaskSection/AddTaskSection';
-import AutoHeight from '../../components/AutoHeight/AutoHeight';
 
 const Tasks = () => {
   return (

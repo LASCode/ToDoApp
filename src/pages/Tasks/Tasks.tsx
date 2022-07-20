a9f855f1640f93a31c0e1aa1868d283941633fca
LASCode <geimixa123@gmail.com>
2022-07-20 23:32:14 +0300

    add tasklist component

--- src/pages/Tasks/Tasks.tsx
+++ src/pages/Tasks/Tasks.tsx
@@ -2,6 +2,7 @@ import './Tasks.scss';
 import React, { useState } from 'react';
 import CrossSvg from '../../assets/img/icon-add.svg';
 import AddTaskSection from './AddTaskSection/AddTaskSection';
+import { TaskList } from '../../UI/Organisms/TaskList/TaskList';
 
 const Tasks = () => {
   return (
@@ -10,7 +11,7 @@ const Tasks = () => {
         <AddTaskSection/>
       </div>
       <div className='TasksPage__taskList'>
-
+        <TaskList />
       </div>
     </div>
   );

d7a04549606eec8bb897fcc9702ebc69062db2ad
LASCode <geimixa123@gmail.com>
2022-07-28 23:35:56 +0300

    server response interfaces refactoring

--- src/types/serverResponses.ts
+++ src/types/serverResponses.ts
@@ -1,30 +1,54 @@
-import { ITask, IServerUserShort } from './entity';
+import { ITask, IServerUserShort, ITask_Server } from './entity';
 
 interface IBaseResponse<T> {
   ok: boolean,
   status: number,
   data: T,
 }
+
 interface IRegisterResponseData {
   success: boolean,
   error: string | false,
   token: string | false,
 }
+
 interface ILoginResponseData {
   success: boolean,
   error: string | false,
   token: string | false,
 }
+
 interface IAuthResponseData {
   success: boolean,
-  error: string | false,
-  userData: IServerUserShort,
+  error: string | null,
+  userData: IServerUserShort | null,
 }
-interface ITasksResponseData {
+
+interface ILogoutUserData<T> {
   success: boolean,
-  error: string | false,
-  tasks: ITask[],
+  error: string | null,
+  userData: T,
+}
+
+interface ITasksResponseData<T> {
+  success: boolean,
+  error: string | null,
+  tasks: T,
+}
+
+interface ITaskResponseSynchronizeData {
+  success: boolean,
+  error: string | null,
+  task: ITask_Server | null,
 }
 
 
-export type { IBaseResponse, IAuthResponseData, ILoginResponseData, ITasksResponseData, IRegisterResponseData };
+export type {
+  IBaseResponse,
+  IAuthResponseData,
+  ILoginResponseData,
+  ITasksResponseData,
+  IRegisterResponseData,
+  ILogoutUserData,
+  ITaskResponseSynchronizeData,
+};

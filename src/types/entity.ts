a12918f7bc8ed0dd620663227f1c69b3b159c0af
LASCode <geimixa123@gmail.com>
2022-07-28 23:34:59 +0300

    refactoring ITask interface, add _Server, _Short, _FormData types

--- src/types/entity.ts
+++ src/types/entity.ts
@@ -6,8 +6,9 @@ interface IServerUser {
   username: string,
   avatar: string,
   tokens: number,
-  tasks: ITask[],
+  tasks: ITask_Server[],
 }
+
 interface IServerUserShort {
   id: number,
   login: string,
@@ -16,18 +17,7 @@ interface IServerUserShort {
   avatar: string,
   tokens: number,
 }
-interface ITask {
-  isSynchronized: boolean,
-  isFetching: boolean,
-  id: number,
-  name: string,
-  description: string,
-  deadline: { isActive: boolean, value: number[] },
-  important: { isActive: boolean, value: number[] },
-  notifications: { isActive: boolean, value: number[] },
-  status: 'Active' | 'Archive' | 'Deleted' | 'Failed',
-  unix: number,
-}
+
 interface IServerDataBase {
   users: IServerUser[],
 }
@@ -40,5 +30,98 @@ interface IReduxAuthData {
   tokens: number,
 }
 
+interface INewTaskTempData {
+  name: string,
+  description: string,
+  isOpen: boolean,
+  deadline: { isActive: boolean, isOpen: boolean, value: number[] },
+  important: { isActive: boolean, isOpen: boolean, value: number[] },
+  notification: { isActive: boolean, isOpen: boolean, value: number[] },
+}
 
-export type { ITask, IServerUser, IServerUserShort, IServerDataBase, IReduxAuthData };
+interface INewTaskTempData_short {
+  name: string,
+  description: string,
+  isOpen: boolean,
+  deadline: { isActive: boolean, value: number[] },
+  important: { isActive: boolean, value: number[] },
+  notification: { isActive: boolean, value: number[] },
+}
+
+
+type TaskActionValue = (null | number);
+type TaskStatusValue = 'Active' | 'Archive' | 'Deleted' | 'Failed';
+interface ITask {
+  id: number,
+  name: string,
+  description: string,
+  deadline: { isActive: boolean, value: TaskActionValue[] },
+  important: { isActive: boolean, value: TaskActionValue[] },
+  notification: { isActive: boolean, value: TaskActionValue[] },
+  status: TaskStatusValue,
+  unix: number,
+  isSynchronized: boolean,
+  isFetching: boolean,
+  error: string | null,
+}
+interface ITask_Short {
+  name: string,
+  description: string,
+  deadline: { isActive: boolean, value: TaskActionValue[] },
+  important: { isActive: boolean, value: TaskActionValue[] },
+  notification: { isActive: boolean, value: TaskActionValue[] },
+}
+interface ITask_Server {
+  id: number,
+  status: TaskStatusValue,
+  unix: number,
+  name: string,
+  description: string,
+  deadline: { isActive: boolean, value: TaskActionValue[] },
+  important: { isActive: boolean, value: TaskActionValue[] },
+  notification: { isActive: boolean, value: TaskActionValue[] },
+}
+interface ITask_FromData {
+  name: string,
+  description: string,
+  isOpen: boolean,
+  deadline: { isActive: boolean, value: TaskActionValue[] },
+  important: { isActive: boolean, value: TaskActionValue[] },
+  notification: { isActive: boolean, value: TaskActionValue[] },
+}
+
+
+interface INewTask {
+  name: string,
+  description: string,
+  isOpen: boolean,
+  deadline: { isActive: boolean, value: (null | number)[] },
+  important: { isActive: boolean, value: (null | number)[] },
+  notification: { isActive: boolean, value: (null | number)[] },
+}
+
+interface INewTask_Storage {
+  name: string,
+  description: string,
+  isOpen: boolean,
+  deadline: { isActive: boolean, value: (null | number)[] },
+  important: { isActive: boolean, value: (null | number)[] },
+  notification: { isActive: boolean, value: (null | number)[] },
+}
+
+
+export type {
+  IServerUser,
+  IServerUserShort,
+  IServerDataBase,
+  IReduxAuthData,
+  INewTaskTempData,
+  INewTaskTempData_short,
+  INewTask,
+  INewTask_Storage,
+
+  ITask,
+  ITask_Server,
+  ITask_FromData,
+  ITask_Short,
+};

ec6157aff175ea6cdfd02db3539c4198872f3cec
LASCode <geimixa123@gmail.com>
2022-07-29 23:16:59 +0300

    add initial account to fake database (admin:admin)

--- src/fakeBackend/variables.ts
+++ src/fakeBackend/variables.ts
@@ -1,8 +1,19 @@
 const initialStorageName = 'DataBase';
 const initialDataBase = {
-  users: [],
+  users: [
+    {
+      "id": 1,
+      "login": "admin",
+      "password": "admin",
+      "authToken": "pge2lbszugh4tw3rp1gv3s",
+      "username": "admin",
+      "avatar": "https://i.ibb.co/w6c7gjL/default-User-Avatar.png",
+      "tokens": 0,
+      "tasks": []
+    }
+  ],
 };
-const initialTimeOut = 500;
+const initialTimeOut = 200;
 
 const defaultUserAvatar = 'https://i.ibb.co/w6c7gjL/default-User-Avatar.png';
 const defaultUserName = 'Неавторизованный пользователь';
